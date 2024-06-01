12ce22a3b579e193c47ffdde2b839940b561397a
Angelo Burzi <angelo.burzi@immobiliare.it>
2024-06-01 16:13:12 +0200

    fixed

--- strapi-root.sh
+++ strapi-root.sh
@@ -1,4 +1,3 @@
-#!/bin/ash
-apk update && \
-apk add curl && \
+#!/bin/bash
+curl http://localhost:1337/
 su node -- /home/node/strapi-start.sh "$@"
--- strapi-start.sh
+++ strapi-start.sh
@@ -1,16 +1,14 @@
-#!/bin/ash
-echo $HOME
-echo $@
-inited=$( /home/node/str-contains.sh "$PATH" "/home/node/.npm-global/bin" )
+#!/bin/bash
+inited=$( $HOME/str-contains.sh "$PATH" "$HOME/.npm-global/bin" )
 if [ "$inited" != "1" ]; then
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y && \
-    source "/home/node/.cargo/env" && \
-    export PATH="$PATH:/home/node/.npm-global/bin" && \
+    source "$HOME/.cargo/env" && \
+    export PATH="$PATH:$HOME/.npm-global/bin" && \
     npm i -g node-gyp cargo-cp-artifact && \
-    mkdir -p /home/node/test2 && cd /home/node/test2 && \
+    mkdir -p "$HOME/test2" && cd "$HOME/test2" && \
     npm i diff-bk || exit 1;
 fi && \
-if [ ! -d /home/node/app/node_modules ]; then
+if [ ! -d "./node_modules" ]; then
     yarn install
 fi && \
 docker-entrypoint.sh "$@"
