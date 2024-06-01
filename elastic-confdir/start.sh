500adadee66f807cd216f293bd262b55e82e92fd
Angelo Burzi <angelo.burzi@immobiliare.it>
2024-06-01 11:54:33 +0200

    updated

--- elastic-confdir/start.sh
+++ elastic-confdir/start.sh
@@ -5,13 +5,11 @@ if [ ! -f /usr/share/elasticsearch/data/index-ready ]; then
     else
         startPass=$ELASTIC_PASSWORD
     fi
-    echo "$startPass"
-    
     startUrl="http://localhost:9200"
     if [[ $startPass != "" ]]; then
         startUrl="http://elastic:$startPass@localhost:9200"
     fi
-    (sleep 60 && \
+    (sleep 120 && \
     for fname in /usr/share/elasticsearch/config/indexes/*.json; do
         fname=${fname##*/}
         curl -XPUT $startUrl/${fname%%.json}  -H "Content-Type: application/json" -d @/usr/share/elasticsearch/config/indexes/$fname
