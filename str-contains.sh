9781fc3f1aaafe98b947079e5303927249691341
Angelo Burzi <angelo.burzi@immobiliare.it>
2024-06-01 14:41:16 +0200

    updated

--- str-contains.sh
+++ str-contains.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
-
-contains() {  if    [ "$1" ] &&            # Is there a source string.
-                    [ "$2" ] &&            # Is there a substring.
-                    [ -z "${1##*"$2"*}" ]; # Test substring in source.
-              then  echo 0;                # Print a "0" for a match.
-              else  echo 1;                # Print a "1" if no match.
-              fi;
-            }
-
-contains "$1" "$2"
+if [ "$1" ] && [ "$2" ] && [ -z "${1##*"$2"*}" ]; then
+    echo 0
+else 
+    echo 1
+fi
