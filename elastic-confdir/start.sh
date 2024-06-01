2c359e56edae4aba5e6bf072c106ac324bc09ca5
Angelo Burzi <angelo.burzi@immobiliare.it>
2024-06-01 11:44:19 +0200

    updated

--- elastic-confdir/start.sh
+++ elastic-confdir/start.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
+echo ciao
 if [ ! -f /usr/share/elasticsearch/data/index-ready ]; then
     if [[ $ELASTIC_PASSWORD_FILE != "" ]]; then
         startPass=$(cat $ELASTIC_PASSWORD_FILE)
     else
         startPass=$ELASTIC_PASSWORD
     fi
+    echo ciao
     startUrl="http://localhost:9200"
     if [[ $startPass != "" ]]; then
         startUrl="http://elastic:$startPass@localhost:9200"
     fi
+    echo ciao
     (sleep 15 && \
     for fname in /usr/share/elasticsearch/config/indexes/*.json; do
         fname=${fname##*/}
         curl -XPUT $startUrl/${fname%%.json}  -H "Content-Type: application/json" -d @/usr/share/elasticsearch/config/indexes/$fname
     done && \
     touch /usr/share/elasticsearch/data/index-ready )&
+    echo ciao
 fi
+echo ciao
 /usr/local/bin/docker-entrypoint.sh eswrapper
