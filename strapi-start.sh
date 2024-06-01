dd810462937c627048b432104f91a3ec2df6d579
Angelo Burzi <angelo.burzi@immobiliare.it>
2024-06-01 15:27:57 +0200

    fixed

--- strapi-start.sh
+++ strapi-start.sh
@@ -5,6 +5,7 @@ su node <<EOSU
 inited=$( /home/node/str-contains.sh "$PATH" "/home/node/.npm-global/bin" )
 if [ "$inited" != "1" ]; then
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y && \
+    source "$HOME/.cargo/env" && \
     export PATH="$PATH:/home/node/.npm-global/bin" && \
     npm i -g node-gyp cargo-cp-artifact && \
     mkdir -p /home/node/test2 && cd /home/node/test2 && \
